e6910ba15c6b0b787f7cc96cb7a2d226dccf2fbc
sophiearana <arana.sophie@gmailcom>
2017-08-23 09:35:07 +0200

    add navbarpage

--- ui.R
+++ ui.R
@@ -18,46 +18,59 @@ css <- ".mandatory_star { color: red; }
           content: 'An error occurred. Please contact the admin.'; }
        "
 
-shinyUI(fluidPage(
-  tags$script("Shiny.addCustomMessageHandler('resetValue', function(variableName) {
+shinyUI(
+  navbarPage(title="Skillshare database",
+    tabPanel("Data",
+      div(class="outer",
+          
+             tagList(
+               tags$head(
+                 tags$link(rel="stylesheet", type="text/css"),#href="style.css"), through css we can edit the text size/font family
+                 tags$script("Shiny.addCustomMessageHandler('resetValue', function(variableName) {
                                             Shiny.onInputChange(variableName, null);});"),
-  tags$head(tags$title('Skillshare database for the IMRPS and Language in Interaction')),
-  useShinyjs(),
-  shinyjs::inlineCSS(css),
-  titlePanel(title=div(img(src="images/combined_logos.png", height= 90), "Skillshare Database")),
-  fluidRow(
-    column(5,
-           tagList(
-             tags$head(
-               tags$link(rel="stylesheet", type="text/css")#href="style.css"), through css we can edit the text size/font family
-             )
-           ),
-           visNetworkOutput("network")),
-    column(7,
-           tabsetPanel(
-             tabPanel("Table",
-                      DT::dataTableOutput("database"), tags$hr()),
-             tabPanel("Add data",  #pop-up when "Add Data" is clicked
-                      actionButton("buttonAdd", "Add Data"),
-                      bsModal("modaladd", "Add data", "buttonAdd", size = "small",
-                              HTML("Please fill in this form and press submit"),
-                              textInput("firstName", labelMandatory("First Name"), ""),
-                              textInput("lastName", labelMandatory("Last Name"), ""),
-                              textInput("email", labelMandatory("Email"), ""),
-                              uiOutput("skillsSelector"),
-                              textInput("skillsDetail", "Skill in detail", ""),
-                              uiOutput("needsSelector"),
-                              textInput("needsDetail", "Need in detail", ""),
-                              uiOutput("departmentSelector"),
-                              actionButton("submit", "Submit"))
-                      ),
-             tabPanel("Keyword Statistics",
-                      splitLayout(#width="100%", #cellHeights = c("100%", "100%"),
-                        plotlyOutput("piePlotSkills"),
-                        plotlyOutput("piePlotNeeds")))
-              )
+                 useShinyjs(),
+                 shinyjs::inlineCSS(css)
+                 )
+             ),
+          fluidRow(
+             column(6,visNetworkOutput("network")),
+            column(6,DT::dataTableOutput("database"), tags$hr())) 
+             ))
     ))
-  # TODO: Move this info to an "About" page
-  #, fluidRow(column(4, helpText('Click on one of the dot to get more details about individual: '), htmlOutput("data_individual")))
-  )
-)
+    
+#   fluidRow(
+#     column(5,
+#            tagList(
+#              tags$head(
+#                tags$link(rel="stylesheet", type="text/css")#href="style.css"), through css we can edit the text size/font family
+#              )
+#            ),
+#            visNetworkOutput("network")),
+#     column(7,
+#            tabsetPanel(
+#              tabPanel("Table",
+#                       DT::dataTableOutput("database"), tags$hr()),
+#              tabPanel("Add data",  #pop-up when "Add Data" is clicked
+#                       actionButton("buttonAdd", "Add Data"),
+#                       bsModal("modaladd", "Add data", "buttonAdd", size = "small",
+#                               HTML("Please fill in this form and press submit"),
+#                               textInput("firstName", labelMandatory("First Name"), ""),
+#                               textInput("lastName", labelMandatory("Last Name"), ""),
+#                               textInput("email", labelMandatory("Email"), ""),
+#                               uiOutput("skillsSelector"),
+#                               textInput("skillsDetail", "Skill in detail", ""),
+#                               uiOutput("needsSelector"),
+#                               textInput("needsDetail", "Need in detail", ""),
+#                               uiOutput("departmentSelector"),
+#                               actionButton("submit", "Submit"))
+#                       ),
+#              tabPanel("Keyword Statistics",
+#                       splitLayout(#width="100%", #cellHeights = c("100%", "100%"),
+#                         plotlyOutput("piePlotSkills"),
+#                         plotlyOutput("piePlotNeeds")))
+#               )
+#     ))
+#   # TODO: Move this info to an "About" page
+#   #, fluidRow(column(4, helpText('Click on one of the dot to get more details about individual: '), htmlOutput("data_individual")))
+#   )
+# )
