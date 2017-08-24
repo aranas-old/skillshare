1d9b669f6177f2cfc68ed33915bfca6b82103e5f
sophiearana <arana.sophie@gmailcom>
2017-08-24 09:01:39 +0200

    change to two collapsible buttons

--- ui.R
+++ ui.R
@@ -36,42 +36,42 @@ shinyUI(
              ),
           fluidRow(
              column(6,visNetworkOutput("network"),
-                    absolutePanel(id = "welcome", class = "panel panel-default", fixed = TRUE
-                    )),
-            column(6,DT::dataTableOutput("database"), tags$hr(),
-                   absolutePanel(id = "welcome", class = "panel panel-default", fixed = TRUE,
-                                 draggable = TRUE, top = 60, left = 20, right = "auto", bottom = "auto", width=300, heigth = "auto",
-                                 style = "opacity: 0.80",
-                                 fluidPage(
-                                   bsCollapse(id = "collapse", open = "How to",
-                                   bsCollapsePanel("How to",
-                                    div('This database is meant to efficiently connect doctoral students by linking the ones who need help to the ones who can offer help - both in work as well as leisure.
+                    absolutePanel(id = "welcome", class = "panel panel-default", fixed = TRUE,
+                                  draggable = FALSE, top = 70, left = 20, right = "auto", bottom = "auto", width=250, heigth = "auto",
+                                  style = "opacity: 0.80",
+                                    bsCollapse(id = "collapsehowto", open = "NULL",
+                                    bsCollapsePanel("How to", open = "NULL",
+                                          div('This database is meant to efficiently connect doctoral students by linking the ones who need help to the ones who can offer help - both in work as well as leisure.
                                           
                                           You can explore our database by interacting with both the network graph or the table. Just click on a circle to learn more about that persons skills & needs! Or explore who???s is helping out by hovering over the arrows!
                                           
                                           Would you like to join? Simply add your data and be part of our network!
                                         
-                                        '),
-                                    actionButton("More", "Show Less")
-                                    )),
-                                   h4("Join the community!"),
-                                   actionButton("buttonAdd", "Add Data"),
-                                   bsModal("modaladd", "Add data", "buttonAdd", size = "small",
-                                   HTML("Please fill in this form and press submit"),
-                                   textInput("firstName", labelMandatory("First Name"), ""),
-                                   textInput("lastName", labelMandatory("Last Name"), ""),
-                                   textInput("email", labelMandatory("Email"), ""),
-                                   uiOutput("skillsSelector"),
-                                   textInput("skillsDetail", "Skill in detail", ""),
-                                   uiOutput("needsSelector"),
-                                   textInput("needsDetail", "Need in detail", ""),
-                                   uiOutput("departmentSelector"),
-                                   actionButton("submit", "Submit")),
-                                   h4("Details:"),
-                                   div('We could also display details in this window this way people could have the details visible while scanning through the table. I am not sure what is better.')
-                                 ))
-                   ) 
-                  )
+                                        ')
+                                               ))),
+                    absolutePanel(id = "join", class = "panel panel-default", fixed = TRUE,
+                                  draggable = FALSE, top = 70, left = 280, right = "auto", bottom = "auto", width=200, heigth = "auto",
+                                  style = "opacity: 0.80",
+                                    bsCollapse(id = "collapsejoin", open = "NULL",
+                                    bsCollapsePanel("Join the Community!",
+                                           actionButton("buttonAdd", "Add your Data"),
+                                           bsModal("modaladd", "Add data", "buttonAdd", size = "small",
+                                                   HTML("Please fill in this form and press submit"),
+                                                   textInput("firstName", labelMandatory("First Name"), ""),
+                                                   textInput("lastName", labelMandatory("Last Name"), ""),
+                                                   textInput("email", labelMandatory("Email"), ""),
+                                                   uiOutput("skillsSelector"),
+                                                   textInput("skillsDetail", "Skill in detail", ""),
+                                                   uiOutput("needsSelector"),
+                                                   textInput("needsDetail", "Need in detail", ""),
+                                                   uiOutput("departmentSelector"),
+                                                   actionButton("submit", "Submit"))
+                                           )
+                                    )
+                                  )),
+            column(6,DT::dataTableOutput("database"), tags$hr()
+                   )
+            )
           )
           ),
     tabPanel("About",
