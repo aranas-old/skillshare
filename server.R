5885f226f2ba5c18cd1471857f0c8f4ac3593672
Chara Tsoukala <x.tsoukala@gmail.com>
2017-09-21 18:19:14 +0200

    only renew color palette if a skill was added/removed

--- server.R
+++ server.R
@@ -8,15 +8,18 @@ library(DT)
 require(shiny)
 library(shinyjs)
 library(shinyBS)
+library(plyr)
 require("RSQLite")
+source("sql_transactions.r")
+
 
 ### Set variables #####
 fields <- c("timestamp", "name","email","skills", "skillsDetail","newskill","needs","needsDetail","newneed","cohort", "affiliation", "location")
-sql_fname = "db/data.sqlite"
 # Create color palette: get all available colors (total: 150 --151 including white)
 col_palts = brewer.pal.info[brewer.pal.info$category != 'seq',]  # Options: div, seq, qual. Get all but sequential ones. 
 color_vector = unique(unlist(mapply(brewer.pal, col_palts$maxcolors, rownames(col_palts))))
 color_vector = color_vector[color_vector != '#FFFFFF']  # remove white
+initial_colors = sample(color_vector, 20, replace=TRUE)  # sample 20 colors
 
 labelMandatory <- function(label) {
   tagList(
@@ -25,14 +28,16 @@ labelMandatory <- function(label) {
   )
 }
 
-function(input, output, session) {
-  clear_form <- function(){
-    updateTextInput(session, "name", value = "")
-    updateTextInput(session, "email", value = "")
-    updateTextInput(session, "skillsDetail", value = "")
-    updateTextInput(session, "needsDetail", value = "")
+### "Detail" button ###
+shinyInput <- function(FUN, len, id, ...) {
+  inputs <- character(len)
+  for (i in seq_len(len)) {
+    inputs[i] <- as.character(FUN(paste0(i), ...))
   }
-  
+  inputs
+}
+
+function(input, output, session) {
   observe({
     ### Add form #####
     formData <- reactive({
@@ -96,7 +101,6 @@ function(input, output, session) {
         saveData(formData())
     }})
       
-    
     observeEvent(input$submitDelete,{
       removeUser(value$current)
       removeModal()
@@ -112,21 +116,6 @@ function(input, output, session) {
       editData(changes, value$current)
       removeModal()  # close pop-up(s) when submit button is clicked #toggleModal(session, "modaledit", toggle = "close")
     })
-    
-    # helper functions to handle text
-    clean_text <- function(x){ uppercase_first(trimws(x)) }
-    clean_list_to_string <- function(x){ paste(clean_text(x), collapse=", ") }
-    uppercase_first <- function(x){  # uppercase the first letter, it's only for aesthetics
-      substr(x, 1, 1) <- toupper(substr(x, 1, 1))
-      x
-    }
-    remove_empty <- function(x){if (!is.null(x)) { x[x != ""] } else x }
-    data_to_list <- function(x){remove_empty(trimws(unlist(strsplit(x, ","))))}
-    string_to_list <- function(x){remove_empty(trimws(unlist(strsplit(x, ","))))}
-    # check the vailidity of the e-mail format:
-    isValidEmail <- function(x) {
-      grepl("\\<[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\>", as.character(x), ignore.case=TRUE)
-    }
     
     ### Table #####
     getBasicInfo <- reactive({
@@ -148,13 +137,12 @@ function(input, output, session) {
       sort(unique(data_to_list(paste(data$skills, ", ", data$needs,", ","!Add your own keyword"))))  # returns all unique keywords (needs + skills)
     })
     
-    
     getColorPalette <- reactive({
-       skills = unique(skillsKeywords())
-       ####palet = colorRampPalette(brewer.pal(length(unique(skills)), "Paired"))   # Pastel1 has less colors
-       ####colors = data.frame(skills = sort(unique(skills)), colors = c(color = palet(length(unique(skills)))))
-       # If you don't like the sampling from all colors solution, we can go back to the brewer (lines above) that has better combinations but only 11 colors max.
-       sample(color_vector, length(skills), replace=TRUE)  # replace will come handy when the number of skills/needs exceeds the num of available colors
+      skills = unique(skillsKeywords())
+      if (length(skills) > length(initial_colors)){
+        initial_colors <<- sample(color_vector, length(skills), replace=TRUE)  # update palette
+      }
+      initial_colors
     })
     
     skillsKeywords <- reactive({
@@ -205,46 +193,43 @@ function(input, output, session) {
       skills = strsplit(data$skills, ", ")
       # Go through Needs instead of Skills, they are less
       for (row in 1:nrow(data)){
-        node_connection_size = 1 # default (arbitrary) connection size. We can use it to determine the size of the node.
+        #node_connection_size = 1 # default (arbitrary) connection size. We can use it to determine the size of the node.
         current_need <- string_to_list(data$needs[row])
         for (need in current_need) {
           skilled_idx <- grep(need, skills, ignore.case = TRUE)
+          skilled_idx = skilled_idx[skilled_idx!=row] # just to avoid the "self-help" loops (some people enter same keyword for skills/needs, e.g. "R")
           if (length(skilled_idx) > 0){
-            node_connection_size = node_connection_size + length(skilled_idx)  # increase by connections of current skill
+            #node_connection_size = node_connection_size + length(skilled_idx)  # increase by connections of current skill
             from = data$rowid[skilled_idx]  # due to deletions, rowid != the number of the row in the db, need to adjust.
             to = rep(data$rowid[row], length(from))
             title = need
             edges <- rbind(edges, as.data.frame(t(rbind(to, from, title))))
           }
         }
+        #edges = rbind(edges, id = seq.int(nrow(edges)))
         nodes <- rbind(nodes, data.frame(id = data$rowid[row],
                                          name = data$name[row],
                                          skills = data$skills[row],
-                                         needs = data$needs[row],
-                                         connection_size = node_connection_size))
+                                         needs = data$needs[row]))
+                                         #,connection_size = node_connection_size))
       }
       #edges <- edges[order(edges$title),]
       list(nodes = nodes, edges = edges) # returns nodes and edges (pairs)
     })
     
-    networkgraph <- reactive({  # Should this be a function instead of reactive?
-      # set visual parameters of graph
+    networkgraph <- reactive({ 
       graphinfo <- node_pairs()
       nodes <- graphinfo$nodes
       edges <- graphinfo$edges
-      nodes$shape <- "dot"
-      nodes$shadow <- TRUE # Nodes will drop shadow
-      nodes$label <- NULL # Node label
+      #### set visual parameters of graph  ####
+      nodes$shadow = TRUE
       nodes$title <- nodes$name
-      nodes$size <- 12 # I actually like uniformity in the nodes, but if you want to play with the size of the connections then: #nodes$connection_size * 3 (or any other number)
-      nodes$font.size <- 0
-      #set nodes colors
+      nodes$size <- 12 # CT: I actually like uniformity in the nodes, but if you want to play with the size of the connections then: #nodes$connection_size * 3 (or any other number)
       nodes$color.background <- "#4bd8c1"
       nodes$color.border <- "#42b2a0"
       nodes$color.highlight.background <- "#4bd8c1"
       nodes$color.highlight.border <- "#006600"
-      #set edges parameters
-      edges$arrows <- "to" # arrows: 'from', 'to', or 'middle'
+      #set edge parameters
       edges$smooth <- FALSE    # should the edges be curved?   -- Chara: I actually like the curved edges and the bouncing effect on loading so I vote for TRUE
       edges$width <- 5 # edge shadow
       visNetwork(nodes, edges) %>%
@@ -257,62 +242,59 @@ function(input, output, session) {
     output$network <- renderVisNetwork({
       networkgraph()
     })
+    
     # If search function in datatable is used, clear selection)
     observeEvent(input$database_rows_all, {
       proxy = dataTableProxy('database')
       selectRows(proxy, NULL)
     })
-    # interaction graph & datatable (both search function updates as well as selection)
+    
     observeEvent(c(input$database_rows_all,input$database_rows_selected), {
-      indx = input$database_rows_all      # rows on all pages (after being filtered)
-      if (!is.null(input$database_rows_selected)){
-         indx = input$database_rows_selected
-      }
-      #access reactive values
-      graphinfo <- node_pairs()
+      graphinfo <- node_pairs()  # access reactive values
       nodes <- graphinfo$nodes
       edges <- graphinfo$edges
       skills = unique(skillsKeywords())
-      colors = data.frame(skills = skills, colors = c(color = getColorPalette()), stringsAsFactors=FALSE)
-      #print(colors$colors)
-      #print(colors$skills)
-      #change color
-      nodes$color.background <- "fff"
-      nodes$color.border <- "#d3d3d3"
-      nodes$color.background[indx] <- "#4bd8c1"
-      nodes$color.border[indx] <- "#42b2a0"
+      colors = data.frame(skills = skills, colors = c(color = getColorPalette()), stringsAsFactors=FALSE) #print(colors$colors) #print(colors$skills)
+      nodes$color.background <- "#4bd8c1"
+      nodes$color.border <- "white" # node border color of unselected nodes
       edges$color <- colors$colors[match(edges$title, colors$skills)]  #color all edges (lines)
-      # Gray out non selected nodes
-      #FIX:so ugly, there must be a more elegant way?
-      rowIDs = getRowIDs()
-      gray_out=intersect(which(edges$from%in%setdiff(edges$from,rowIDs[indx])),which(edges$to%in%setdiff(edges$to,rowIDs[indx])))
-      edges$color[gray_out] <- "fff"
-      edges$width <- 5    # edge shadow
+      edges$width <- 5  # edge shadow
       edges$arrows <- "to"
+      indx = input$database_rows_all  # (filtered) rows on all pages
+      if (!is.null(input$database_rows_selected)){
+        indx = input$database_rows_selected
+        # Gray out non selected edges (those that belong to nodes that have not been selected)
+        rowIDs = getRowIDs()
+        non_selected = intersect(which(edges$from%in%setdiff(edges$from,rowIDs[indx])),which(edges$to%in%setdiff(edges$to,rowIDs[indx])))
+        #edges = edges[!rownames(edges) %in% non_selected,]  # part of the attempt to remove edges instead of "graying them out"
+        #removed_edges = edges[rownames(edges) %in% non_selected,]
+        #visNetworkProxy("network") %>%
+        #    visRemoveEdges(id=rownames(removed_edges))  # visRemoveEdges(removed_edges) didn't seem to work either
+        edges$color[non_selected] <- "fff"
+      }
+      nodes$color.border[indx] <- "#42b2a0"  # border color of selected nodes
       # Update network
       visNetworkProxy("network") %>%
-      visUpdateNodes(nodes) %>%
-      visUpdateEdges(edges)
+        visUpdateNodes(nodes) %>%
+        visUpdateEdges(edges)
     })
-    
+
     ### pie plots #####
-    #TODO: plots should update together with graph and table when new data is submitted
     output$piePlotSkills <- renderPlotly({
       skills = skillsKeywords()
       frequencies <-as.data.frame(table(skills))
       plot_ly(frequencies, labels = ~skills, values = ~Freq, type = 'pie',
               textposition = 'inside',
               textinfo = 'percent',
-              #insidetextfont = list(color='#FFFFFF'), default color is black
               hoverinfo = 'text',
               text = ~skills,
               marker = list(colors = c(color = brewer.pal(length(skills),"Paired")), # colors do not correspond to colors in network graph
                             line = list(color = '#FFFFFF', width = 2)),
               showlegend = TRUE
       ) %>%
-        layout(title = 'Skills',
-               xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-               yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+      layout(title = 'Skills',
+             xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+             yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
     })
     
     output$piePlotNeeds <- renderPlotly({
@@ -327,9 +309,9 @@ function(input, output, session) {
                             line = list(color = '#FFFFFF', width = 2)),
               showlegend = TRUE
       ) %>%
-        layout(title = 'Needs',
-               xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-               yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+      layout(title = 'Needs',
+             xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+             yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
     })
     
     ### Detailed view of user#####
@@ -409,71 +391,9 @@ function(input, output, session) {
     observeEvent(input$buttonDelete,{
       userInfo <- queryUserInfo(value$current)
       showModal(modalDialog(
-        title = sprintf("Are you sure you want to delete all data for: %s (%s)", userInfo$name, userInfo$email, " ?"),
+        title = sprintf("Are you sure you want to delete all data for: %s (%s)?", userInfo$name, userInfo$email),
         footer = tagList(modalButton("No, cancel"), actionButton("submitDelete", "Confirm & Delete")))
       )
     })
-    
-    
-    ### SQL Lite database transactions ###
-    userExists <- function(name, email){
-      con <- dbConnect(SQLite(), sql_fname)
-      data <- dbGetQuery(con, sprintf("SELECT EXISTS(SELECT 1 FROM skillshare WHERE name='%s' AND email='%s');", name, email))
-      dbDisconnect(con)
-      as.logical(data)
-    
-    }
-    
-    queryUserInfo <- function(user_id) {
-      sql_db <- dbConnect(SQLite(), sql_fname)
-      data <- dbGetQuery(sql_db, sprintf("SELECT * FROM skillshare WHERE rowid = %s", user_id))
-      dbDisconnect(sql_db)
-      data
-    }
-    
-    queryBasicData <- function() {
-      sql_db <- dbConnect(SQLite(), sql_fname)
-      data <- dbGetQuery(sql_db, "SELECT rowid, name, skills, needs FROM skillshare")
-      dbDisconnect(sql_db)
-      colnames(data) <- c("rowid","name", "skills", "needs")
-      data <- data[order(data$name),]
-      rownames(data) <- 1:nrow(data)
-      data
-    }
-    
-    removeUser <- function(user_id) {
-      sql_db <- dbConnect(SQLite(), sql_fname)
-      dbExecute(sql_db, sprintf("DELETE FROM skillshare WHERE rowid = %s", user_id))
-      dbDisconnect(sql_db)
-    }
-    
-    editData <- function(values, user_id){
-      query <- sprintf("UPDATE skillshare SET %s WHERE rowid = %s", values, user_id)
-      sql_db <- dbConnect(SQLite(), sql_fname)
-      dbExecute(sql_db, query)
-      dbDisconnect(sql_db)
-    }
-    
-    saveData <- function(data) {
-      # Clean data (uppercase etc) before saving
-      name = clean_text(data$name)
-      skills = clean_list_to_string(data$skills)
-      needs = clean_list_to_string(data$needs)
-      query <- sprintf("INSERT INTO skillshare VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', CURRENT_TIMESTAMP)", 
-                       name, trimws(data$email), skills, clean_text(data$skillsDetail), needs, clean_text(data$needsDetail), data$cohort, data$affiliation, data$location
-                       )
-      sql_db <- dbConnect(SQLite(), sql_fname)
-      dbExecute(sql_db, query)
-      dbDisconnect(sql_db)
-    }
-    
-    # "Detail" button
-    shinyInput <- function(FUN, len, id, ...) {
-      inputs <- character(len)
-      for (i in seq_len(len)) {
-        inputs[i] <- as.character(FUN(paste0(i), ...))
-      }
-      inputs
-    }
   })
 }
