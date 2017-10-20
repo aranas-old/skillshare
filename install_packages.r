a9e786679bd15cd913512a21b1058bedefccf2c3
Chara Tsoukala <x.tsoukala@gmail.com>
2017-10-20 10:27:53 +0200

    install required packages from terminal

---
+++ install_packages.r
@@ -0,0 +1,2 @@
+install.packages(c("shiny", "shinyjs", "shinyBS", "RSQLite", "DT", "visNetwork", "plotly", "igraph", "RColorBrewer", "devtools"), repos = "http://cran.us.r-project.org")
+devtools::install_github("AnalytixWare/ShinySky")
